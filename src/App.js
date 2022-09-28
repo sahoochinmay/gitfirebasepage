d0127355a09687f5b76f4a1ddfa3eb35d78154a9
sahoochinmay <sahoochinmay54@gmail.com>
2022-09-28 08:58:29 +0530

    fetch food data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { foodsRef } from './config/firebase';
+import logo from "./logo.svg";
+import "./App.css";
+import { foodsRef } from "./config/firebase";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetchFood();
+  }, []);
+
+  const fetchFood = async () => {
+    let arr = [];
+    foodsRef
+      .get()
+      .then((docs) => {
+        docs.forEach((doc) => {
+          arr.push(doc.data());
+        });
+        console.log(arr)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +29,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
